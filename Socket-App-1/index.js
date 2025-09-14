2a34c9a96fc56ee5b71ab0ffaef0b5cf96f3791e
tashrif-miyaji <tashrifmiyaji@gmail.com>
2025-09-14 14:48:37 +0600

    socket config

--- Socket-App-1/index.js
+++ Socket-App-1/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const server = http.createServer(app);
+const expressServer = http.createServer(app);
+const io = new Server(expressServer);
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname+"/index.html");
+	res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000, () => {
+io.on("connection", (socket)=>{
+	console.log("new user connected" + ", usr id :" + socket.id);
+	
+	socket.on("disconnect", ()=>{
+		console.log("user disconnected"+ ", usr id :" + socket.id);
+		
+	})
+})
+
+expressServer.listen(3000, () => {
 	console.log("server listening on port 3000");
 });
