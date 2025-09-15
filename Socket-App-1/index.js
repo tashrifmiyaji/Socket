0d5ae10f54fbeee3110f43fb2ada9b37133b7b55
tashrif-miyaji <tashrifmiyaji@gmail.com>
2025-09-15 13:09:49 +0600

    send data client to server with socket pre-defined event

--- Socket-App-1/index.js
+++ Socket-App-1/index.js
@@ -13,18 +13,23 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
 	console.log("new user connected" + ", usr id :" + socket.id);
 
-	(() => {
-		const message = ["hello everyone", "hello tashrif", "hello clint"];
-		let displayMessage = null;
-		let index = 0;
-		setInterval(() => {
-			displayMessage = message[index];
-			index = (index + 1) % message.length;
+	// (() => {
+	// 	const message = ["hello everyone", "hello tashrif", "hello clint"];
+	// 	let displayMessage = null;
+	// 	let index = 0;
+	// 	setInterval(() => {
+	// 		displayMessage = message[index];
+	// 		index = (index + 1) % message.length;
 
-			// socket.send(displayMessage);
-			socket.emit("myEvent", displayMessage)
-		}, 2000);
-	})();
+	// 		// socket.send(displayMessage);
+	// 		socket.emit("myEvent", displayMessage)
+	// 	}, 2000);
+	// })();
+
+	socket.on("message", (msg)=>{
+		console.log(msg);
+		
+	})
 
 	socket.on("disconnect", () => {
 		console.log("user disconnected" + ", usr id :" + socket.id);
