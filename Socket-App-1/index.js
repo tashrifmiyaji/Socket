dba000649ba2fe6f1df4437b06f845b009b3f3d8
tashrif-miyaji <tashrifmiyaji@gmail.com>
2025-09-24 11:26:57 +0600

    socket name space

--- Socket-App-1/index.js
+++ Socket-App-1/index.js
@@ -10,39 +10,49 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-	console.log("new user connected" + ", usr id :" + socket.id);
+// io.on("connection", (socket) => {
+// 	console.log("new user connected" + ", usr id :" + socket.id);
 
-	// (() => {
-	// 	const message = ["hello everyone", "hello tashrif", "hello clint"];
-	// 	let displayMessage = null;
-	// 	let index = 0;
-	// 	setInterval(() => {
-	// 		displayMessage = message[index];
-	// 		index = (index + 1) % message.length;
+// 	// (() => {
+// 	// 	const message = ["hello everyone", "hello tashrif", "hello clint"];
+// 	// 	let displayMessage = null;
+// 	// 	let index = 0;
+// 	// 	setInterval(() => {
+// 	// 		displayMessage = message[index];
+// 	// 		index = (index + 1) % message.length;
 
-	// 		// socket.send(displayMessage);
-	// 		socket.emit("myEvent", displayMessage)
-	// 	}, 2000);
-	// })();
+// 	// 		// socket.send(displayMessage);
+// 	// 		socket.emit("myEvent", displayMessage)
+// 	// 	}, 2000);
+// 	// })();
 
-	// socket.on("message", (msg)=>{
-	// 	console.log(msg);
-		
-	// })
+// 	// socket.on("message", (msg)=>{
+// 	// 	console.log(msg);
 
-	// // custom event
-	// socket.on("myMsgEvent", (msg)=>{
-	// 	console.log(msg);
-	// })
+// 	// })
 
-	// broad cast
-	io.sockets.emit("myBroadCast", "hello everyone!")
+// 	// // custom event
+// 	// socket.on("myMsgEvent", (msg)=>{
+// 	// 	console.log(msg);
+// 	// })
 
+// 	// // broad cast
+// 	// io.sockets.emit("myBroadCast", "hello everyone!")
 
-	socket.on("disconnect", () => {
-		console.log("user disconnected" + ", usr id :" + socket.id);
-	});
+// 	socket.on("disconnect", () => {
+// 		console.log("user disconnected" + ", usr id :" + socket.id);
+// 	});
+// });
+
+// name space
+const sellNsp = io.of("/sell");
+sellNsp.on("connect", (socket) => {
+	socket.emit("myEvent", "lets sell our products.");
+});
+
+const buyNsp = io.of("/buy");
+buyNsp.on("connect", (socket) => {
+	socket.emit("myEvent", "lets buy products");
 });
 
 expressServer.listen(3000, () => {
